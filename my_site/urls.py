66d32646076aea47d5624168ac6513556a7eeb28
OSAMATAMER <59906580+OSAMATAMER@users.noreply.github.com>
2023-05-22 21:45:50 +0300

    requirements.txt

--- my_site/urls.py
+++ my_site/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from . import views
 from .views import CountListView
 
-app_name = 'my_site'
 urlpatterns = [
     path('', CountListView.as_view() , name='index'),
-    path('my_site', CountListView.as_view(), name='my_site'),
+    path('my_site', CountListView.as_view(), name='main'),
     path('count/<int:count_id>/', views.count, name='count'),
     path('count/<int:count_id>/delete/', views.delete, name='delete'),
     path('count/<int:count_id>/update/', views.update, name='update'),
